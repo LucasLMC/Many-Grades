e8e51bc6e3a1b87f788efad69cb5c4daad220767
LucasLMC <lucaslmc13@outlook.com>
2021-09-27 15:51:03 -0300

    add icon delete

--- src/components/NoteCard/noteCard.jsx
+++ src/components/NoteCard/noteCard.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./style.css";
+import { ReactComponent as DeleteIcon } from "../../assets/img/delete.svg"
 class NoteCard extends Component {
   render() {
     return (
       <section className="note-card">
         <header className="note_card_header">
           <h3 className="note_card_title">{this.props.title}</h3>
+          <DeleteIcon/>
         </header>
         <p className="note_card_text">{this.props.text}</p>
       </section>
